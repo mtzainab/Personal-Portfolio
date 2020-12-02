22c27c32152076820225909a6a6aefd42e5277a3
mtzainab <preetyzee9911@gmail.com>
2020-12-02 17:17:39 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,127 @@
+const menu = document.querySelector('.menu')
+const navOpen = document.querySelector('.hanburger')
+const navClose = document.querySelector('.close')
+const navBar = document.querySelector('.nav')
+
+const navLeft = menu.getBoundingClientRect().left;
+navOpen.addEventListener('click', ()=>{
+    if (navLeft < 0){
+        menu.classList.add('show');
+        document.body.classList.add('show');
+        navBar.classList.add('show')
+    }
+});
+navOpen.addEventListener('click', ()=>{
+if (navLeft < 0){
+    menu.classList.add('show');
+    document.body.classList.add('show');
+    navBar.classList.remove('show')
+}
+});
+
+// Fix Nav
+const navHeight = navBar.getBoundingClientRect().height;
+window.addEventListener('scroll', ()=>{
+    const scrollHeight = window.pageYOffset;
+    if (scrollHeight > navHeight){
+        navBar.classList.add('fix-nav')
+    }else{
+        navBar.classList.remove('fix-nav')
+    }
+})
+
+//Scroll To
+const links = [...document.querySelectorAll(".scroll-link")];
+links.map(link=>{
+    link.addEventListener('click', e=>{
+        e.preventDefault();
+
+        const id = e.target.getAttribute("href").slice(1);
+        const el = document.getElementById(id);
+        let position = el.offsetTop - navHeight;
+
+        window.scrollTo({
+            top: position,
+            left: 0,
+        });
+        navBar.classList.remove("show");
+        menu.classList.remove("show");
+        document.body.classList.remove("show");
+    });
+});
+
+new TypeIt('#type1',{
+    speed: 120,
+    loop: true,
+    waitUntilVisible: true,
+}).type('Designer', { delay: 400})
+.pause(500)
+.delete(9)
+.type('Developer',{ delay: 400})
+.pause(500)
+.delete(9)
+.go();
+
+new TypeIt('#type2',{
+    speed: 120,
+    loop: true,
+    waitUntilVisible: true,
+}).type('Designer', { delay: 400})
+.pause(500)
+.delete(9)
+.type('Developer',{ delay: 400})
+.pause(500)
+.delete(9)
+.go();
+//GSAP
+gsap.from(".logo", { opasity: 0, duration:1, delay: 0.5, y:-10, });
+gsap.from(".hamburger", { opasity: 0, duration:1, delay: 1, x:20, });
+gsap.from(".banner", { opasity: 0, duration:1, delay: 1.5, x:-200, });
+gsap.from(".hero h3", { opasity: 0, duration:1, delay: 2, y:-50, });
+gsap.from(".hero h1", { opasity: 0, duration:1, delay: 2.5, y:-45, });
+gsap.from(".hero h4", { opasity: 0, duration:1, delay: 3, y:-30, });
+gsap.from(".hero a", { opasity: 0, duration:1, delay: 3.5, y:50, });
+gsap.from(".nav-item", { 
+    opasity: 0, 
+    duration:1, 
+    delay: 1.2,
+     y: 30, 
+     stagger: 0.2,
+     });
+
+     gsap.from(".icons span", { 
+        opasity: 0, 
+        duration:1, 
+        delay: 4,
+         x: -30, 
+         stagger: 0.2,
+         }); 
+         
+ // Glidejs
+
+ const glide = document.querySelector(".glide");
+
+ if(glide)
+ new Glide(glide, {
+     type: 'carousel',
+     startAt: 0,
+     perView: 3,
+     gap: 30,
+     hoverpause: true,
+     autoplay: 2000,
+     animationDuration: 800,
+     animationTimingFunc: "ease-in-out",
+     brakpoints: {
+         996:{
+             perView: 2
+         },
+         768:{
+            perView: 1
+        },
+
+     },
+
+ }).mount();
+ AOS.init()
+ 
+         
